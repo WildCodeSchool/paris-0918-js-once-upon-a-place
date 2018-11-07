6d58d929211cfc8202c28329cc6cdbe76f3f3e2b
ChristopheRibault <c.ribault.dev@gmail.com>
2018-11-07 14:18:37 +0100

    miseen page Team

--- src/Team.js
+++ src/Team.js
@@ -1,31 +1,28 @@
-import React, {Component} from 'react';
-import DisplayTeam from './DisplayTeam';
-import  { TeamList } from './TeamList';
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import DisplayTeam from "./DisplayTeam";
+import { TeamList } from "./TeamList";
+import Header from "./Header";
 
+import "./Team.css";
 
-class Team extends Component{
-    componentDidMount() {
-        this.props.setFooterColor('white');
-    };
-    
-    render(){
-        return(
-            TeamList.map(member => {
-                return (
-                    <div className="list">
-                        <DisplayTeam
-                            key={member.id}
-                            name={member.name}
-                            img={member.img}
-            
-                        />
-                    </div>
-                )
-            }
+class Team extends Component {
+  componentDidMount() {
+    this.props.setFooterColor("white");
+  }
 
-        ))
-    }
+  render() {
+    return (
+      <div className="Team">
+        <Link to='/'><Header /></Link>
+        <div className="team-members">
+          {TeamList.map(member => (
+            <DisplayTeam key={member.id} name={member.name} img={member.img} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Team;
-
